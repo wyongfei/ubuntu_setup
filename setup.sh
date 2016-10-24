3abf6c4fb96ceba40a0e100c19c68e44aa58198c
wyongfei <wyongfei@163.com>
2016-10-23 23:16:00 -0700

    add setup.sh
    Signed-off-by: wyongfei <wyongfei@163.com>

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git config --global user.name "wyongfei"
+git config --global user.email wyongfei@163.com
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gnome-session-fallback
+sudo apt-get install vim
+curl https://raw.githubusercontent.com/wyongfei/spf13-vim/3.0/bootstrap.sh -L > spf13-vim.sh && sh spf13-vim.sh
