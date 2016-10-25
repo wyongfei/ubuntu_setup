2b20b65b0c9afe73e5a9ee299f940385c6c087e6
王永飞 <wyongfei@163.com>
2016-10-25 14:58:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ sudo apt-get upgrade
 sudo apt-get install vim
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
-sudo apt install vim-nox-py2
+#sudo apt install vim-nox-py2
 ./spf13-vim.sh
 #~/.vim/bundle/YouCompleteMe/install.py --clang-completer
