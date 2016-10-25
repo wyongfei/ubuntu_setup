52db9982c983bb911dcd87997bfeb934828e2274
王永飞 <wyongfei@163.com>
2016-10-25 14:57:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 git config --global user.name "wyongfei"
 git config --global user.email wyongfei@163.com
-sudo add-apt-repository ppa:jonathonf/vim
+#sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install gnome-session-fallback
+#sudo apt-get install gnome-session-fallback
 sudo apt-get install vim
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
